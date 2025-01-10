92273e1c05df4ccd150f76101f3cf35e1efb6c83
Ifeoluwa joshua <ifeoluwajohz@gmail.com>
2025-01-10 01:35:38 -0800

    v_1 correacrtion 2

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -51,7 +51,8 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const token = localStorage.getItem("jwt");
 
 
-  const API_URL = "http://localhost:5000";
+  // const API_URL = "https://theevent-i5i1.onrender.com";
+  const API_URL = "https://theevent-i5i1.onrender.com"
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
